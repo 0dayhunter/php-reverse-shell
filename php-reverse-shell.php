e50b6cb5b358011c6464ea11aa53eb0735a76379
0dayhunter <78475739+0dayhunter@users.noreply.github.com>
2023-08-31 17:21:43 +0530

    Update

--- php-reverse-shell.php
+++ php-reverse-shell.php
@@ -1,6 +1,5 @@
 <?php
 // php-reverse-shell - A Reverse Shell implementation in PHP
-// Copyright (C) 2007 pentestmonkey@pentestmonkey.net
 //
 // This tool may be used for legal purposes only.  Users take full responsibility
 // for any actions performed using this tool.  The author accepts no liability
@@ -25,9 +24,6 @@
 // This tool may be used for legal purposes only.  Users take full responsibility
 // for any actions performed using this tool.  If these terms are not acceptable to
 // you, then do not use this tool.
-//
-// You are encouraged to send comments, improvements or suggestions to
-// me at pentestmonkey@pentestmonkey.net
 //
 // Description
 // -----------
